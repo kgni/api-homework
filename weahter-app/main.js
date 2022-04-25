3807e9f0e43c6323ea668aa257ae43f98fbb6451
kgni <admin@mdia.dk>
2022-04-25 16:56:34 +0200

    add functionality for fetching the current weather

--- weahter-app/main.js
+++ weahter-app/main.js
@@ -4,38 +4,11 @@ const currentCityLocation = document.querySelector('.current-city-location');
 const currentTemp = document.querySelector('.current-temp');
 const currentIcon = document.querySelector('.current-icon');
 const weatherDescription = document.querySelector('.weather-description');
+const windText = document.querySelector('.wind');
+const humidText = document.querySelector('.humid');
 
 searchBtn.addEventListener('click', fetchWeather);
 const API_KEY = '0a0a00077254b920fcee144964349838';
-
-async function fetchWeather() {
-	// Saving the data from our fetchLatLon() function.
-	const location = await fetchLatLon();
-
-	// destructuring the data, for us to use in a new fetch
-	const [lat, lon, searchInput] = location;
-
-	// fetching the weather data providing a specific lat and lon.
-	const resWeather = await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-	);
-	const dataWeather = await resWeather.json();
-	console.log(dataWeather);
-
-	// destructuring the data we receive back from our fetch (after we parse)
-	let { name, main, wind, weather } = dataWeather;
-
-	// Inserting data into the DOM.
-	currentCity.innerText = `${searchInput},`;
-	currentCityLocation.innerText = ` ${name}`;
-	currentTemp.innerText = `${main.temp.toFixed(1)} °C`;
-	currentIcon.src = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png
-	`;
-
-	weatherDescription.innerText = weather[0].main;
-
-	console.log(weather.icon);
-}
 
 // In order for us to fetch the weather, we need lattitude and longtitude for the city/location we are inputting.
 
@@ -54,4 +27,52 @@ async function fetchLatLon() {
 
 	// returning the lat, lon and searchInput variables which will be used in our other function to fetch the weather.
 	return [lat, lon, searchInput];
+}
+
+async function fetchWeather() {
+	// Saving the data from our fetchLatLon() function.
+	const location = await fetchLatLon();
+
+	// destructuring the data, for us to use in a new fetch
+	const [lat, lon, searchInput] = location;
+
+	// fetching the weather data providing a specific lat and lon.
+	const resWeather = await fetch(
+		`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+	);
+	const dataWeather = await resWeather.json();
+	console.log(dataWeather);
+
+	// destructuring the data we receive back from our fetch (after we parse)
+	let { name, main, wind, weather } = dataWeather;
+
+	// Formatting searchInput
+
+	// Inserting data into the DOM.
+	currentCity.innerText = `${searchInput}`;
+	currentTemp.innerText = `${main.temp.toFixed(0)}°`;
+	currentIcon.src = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png
+	`;
+
+	weatherDescription.innerText = weather[0].main;
+	windText.innerText = `${wind.speed.toFixed(1)} m/s`;
+	humidText.innerText = `${main.humidity} %`;
+}
+
+// TODO - Fetch forecast
+
+async function fetchForecast() {
+	// Saving the data from our fetchLatLon() function.
+	const location = await fetchLatLon();
+
+	// destructuring the data, for us to use in a new fetch
+	const [lat, lon] = location;
+	const resForecast = await fetch(
+		`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${API_KEY}&units=metric`
+	);
+
+	const dataForecast = await resForecast.json();
+
+	const day1 = new Date(dataForecast.daily[0].dt);
+	console.log(day1.toLocaleString('en-us', { weekday: 'long' }));
 }
